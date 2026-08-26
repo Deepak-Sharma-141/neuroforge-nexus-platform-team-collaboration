553115b14ea16cefc23ef60ddafb9aa76dc9380d
Deepak-Sharma-141 <dsharma19963006@gmail.com>
2026-08-26 22:28:03 +0530

    added the base structure of the project

---
+++ backend/src/main/java/com/neuroforge/neuroforge/nexus/NeuroforgeNexusApplication.java
@@ -0,0 +1,13 @@
+package com.neuroforge.neuroforge.nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroforgeNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuroforgeNexusApplication.class, args);
+	}
+
+}
