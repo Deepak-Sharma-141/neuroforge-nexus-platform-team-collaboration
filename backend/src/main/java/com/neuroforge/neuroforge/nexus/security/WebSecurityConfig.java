4da10bcb28695ee0a75734d09dfbe653cda0b8cc
Deepak-Sharma-141 <dsharma19963006@gmail.com>
2026-08-31 00:11:54 +0530

    added cors for forntend connection

--- backend/src/main/java/com/neuroforge/neuroforge/nexus/security/WebSecurityConfig.java
+++ backend/src/main/java/com/neuroforge/neuroforge/nexus/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,6 +29,7 @@ public class WebSecurityConfig {
         return httpSecurity
                 .securityMatcher(JWT_ROUTES)
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(
                         SessionCreationPolicy.STATELESS
                 ))
